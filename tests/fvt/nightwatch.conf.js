9fbb33bfad81dbc6d2c41b6ad2b6762de97b4117
Alex Grant <alexgrant0@gmail.com>
2020-06-01 03:09:28 +0000

    Removed port variable for testing

--- tests/fvt/nightwatch.conf.js
+++ tests/fvt/nightwatch.conf.js
@@ -14,8 +14,7 @@ module.exports = {
 
   webdriver : {
     "start_process": true,
-    "server_path": "node_modules/.bin/chromedriver",
-    "port": 9515
+    "server_path": "node_modules/.bin/chromedriver"
   },
 
   live_output: true,
