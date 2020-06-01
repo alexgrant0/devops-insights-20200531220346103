96b5ebc165fe52b1292ef678515504c9ddae591d
Alex Grant <alexgrant0@gmail.com>
2020-06-01 02:02:45 +0000

    Fixed unexpected ;

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -51,7 +51,7 @@ function Zip(props) {
 			    		map = new google.maps.Map(document.getElementById('map'), {
 			          		center: {lat: -34.397, lng: 150.644},
 			          		zoom: 8
-			     		});
+			     		})
 			      	}
 			    </script>
             </div>
