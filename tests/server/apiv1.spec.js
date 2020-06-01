6249f6126ea6bc469baef369f3471ae287ecec37
Alex Grant <alexgrant0@gmail.com>
2020-06-01 08:41:59 +0000

    Changes

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -79,7 +79,7 @@ describe('Get Weather', function() {
         city: "Auckland"
       }
     };
-    /*
+    
     const body = {
       cod: 200,
       name: 'Auckland'
@@ -98,11 +98,11 @@ describe('Get Weather', function() {
     };
 
     apiv1.__set__("request", request);
-	*/
+	
     apiv1.getWeather(reqMock, resMock);
-
+    
     assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
-    assert(resMock.send.args[0].city === 'Auckland', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+    assert(resMock.send.lastCall.args[0].city === 'Auckland', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
     //assert(resMock.send.lastCall.args[0].weather === 'Conditions are light rain and temperature is 15.48 C', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
   });
   
