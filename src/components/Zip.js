004580a960515a1d756a9856ca3d53713761f36f
Alex Grant <alexgrant0@gmail.com>
2020-06-01 23:04:57 +0000

    Removed CSS styling

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -51,21 +51,6 @@ function Zip(props) {
 			      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbZSDbWTExhP6Ojrqi08miAFk4Bh_51C0&callback=initMap&libraries=&v=weekly"
 			      defer
 			    ></script>
-			    <style type="text/css">
-			      /* Always set the map height explicitly to define the size of the div
-			       * element that contains the map. */
-			      map {
-			        height: 100%;
-			      }
-			
-			      /* Optional: Makes the sample page fill the window. */
-			      html,
-			      body {
-			        height: 100%;
-			        margin: 0;
-			        padding: 0;
-			      }
-			    </style>
 			    <script>
 			      (function(exports) {
 			        "use strict";
