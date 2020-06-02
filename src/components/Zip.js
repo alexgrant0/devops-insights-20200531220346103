7590ebaf22b720cc3face3c9ab8b7e62c69f0d4d
Alex Grant <alexgrant0@gmail.com>
2020-06-02 03:53:35 +0000

    Changed maps functionality

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -43,29 +43,41 @@ function Zip(props) {
 	            <div className="pl-3 row">
 	                <div className="text-danger small">{ validationError }</div>
 	            </div>
-	        <div/>
-	            
-	            <div id="map"></div>
-	            
-				            // Create the script tag, set the appropriate attributes
-				var script = document.createElement('script');
-				script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
-				script.defer = true;
-				script.async = true;
-				
-				// Attach your callback function to the `window` object
-				window.initMap = function() {
-				  // JS API is loaded and available
-				};
-				
-				// Append the 'script' element to 'head'
-				document.head.appendChild(script);
-				
-				
-				map = new google.maps.Map(document.getElementById('map'), {
-				  center: {lat: -34.397, lng: 150.644},
-				  zoom: 8
-				});
+	        </div>
+			<div>
+				<head>
+				    <title>Simple Map</title>
+				    <meta name="viewport" content="initial-scale=1.0">
+				    <meta charset="utf-8">
+				    <style>
+				      /* Always set the map height explicitly to define the size of the div
+				       * element that contains the map. */
+				      #map {
+				        height: 100%;
+				      }
+				      /* Optional: Makes the sample page fill the window. */
+				      html, body {
+				        height: 100%;
+				        margin: 0;
+				        padding: 0;
+				      }
+				    </style>
+				  </head>
+				  <body>
+				    <div id="map"></div>
+				    <script>
+				      var map;
+				      function initMap() {
+				        map = new google.maps.Map(document.getElementById('map'), {
+				          center: {lat: -34.397, lng: 150.644},
+				          zoom: 8
+				        });
+				      }
+				    </script>
+				    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+				    async defer></script>
+				  </body>
+			</div>
 	            
 	    </div>
     );
