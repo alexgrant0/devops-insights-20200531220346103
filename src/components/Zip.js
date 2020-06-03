64045cf419b98daf528a1552f88317827a4052a0
Alex Grant <alexgrant0@gmail.com>
2020-06-03 07:11:33 +0000

    Commented out iframe, uncommented embed

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -43,8 +43,8 @@ function Zip(props) {
 	            <div className="pl-3 row">
 	                <div className="text-danger small"> { validationError } </div>
 	            </div>
-	            <iframe src="/Map.html" width="500" height="300"></iframe>
-	            //<embed type="text/html" src="Map.html" width="500" height="200"/>
+	            //<iframe src="Map.html" width="500" height="300"></iframe>
+	            <embed type="text/html" src="Map.html" width="500" height="200"/>
 	        </div>
     );
 }
