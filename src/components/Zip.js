1f8da13e0b56eeb6fad29bc09455339508fc9602
Alex Grant <alexgrant0@gmail.com>
2020-06-03 03:54:58 +0000

    Added maps functionality

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -42,6 +42,18 @@ function Zip(props) {
             <div className="pl-3 row">
                 <div className="text-danger small"> { validationError } </div>
             </div>
+            <div id="map"></div>
+		    <script>
+		      var map;
+		      function initMap() {
+		        map = new google.maps.Map(document.getElementById('map'), {
+		          center: {lat: -34.397, lng: 150.644},
+		          zoom: 8
+		        });
+		      }
+		    </script>
+		    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+		    async defer></script>
         </div>
     );
 }
