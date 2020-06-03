24b18ef65338eef76bf2e2c4c226e17997ba1cfd
Alex Grant <alexgrant0@gmail.com>
2020-06-03 14:16:28 +0000

    Changed filename back to map

--- src/components/AppMap.js
+++ src/components/AppMap.js
@@ -4,7 +4,7 @@ function AppMap() {
 
     return (
 	    <div>
-	      <embed type="text/html" src="mapFile.html" width="100%" height="500"/>
+	      <embed type="text/html" src="map.html" width="100%" height="500"/>
 	    </div> 
     );
     
