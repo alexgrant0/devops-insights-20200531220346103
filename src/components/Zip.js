bb39c9d28ef07a3d44780fc9afc9ecba2c3103c9
Alex Grant <alexgrant0@gmail.com>
2020-06-03 04:01:19 +0000

    Removed semicolon

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -49,7 +49,7 @@ function Zip(props) {
 		        map = new google.maps.Map(document.getElementById('map'), {
 		          center: {lat: -34.397, lng: 150.644},
 		          zoom: 8
-		        });
+		        })
 		      }
 		    </script>
 		    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
