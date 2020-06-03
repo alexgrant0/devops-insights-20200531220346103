2eaa497090d5e618b9b1e97b05296fcc0cff5d8d
Alex Grant <alexgrant0@gmail.com>
2020-06-03 12:01:47 +0000

    Changes to map.html and map.js

--- src/components/AppMap.js
+++ src/components/AppMap.js
@@ -4,7 +4,7 @@ import React from 'react';
 function AppMap() {
 
     return <div>
-      <embed type="text/html" src="Map.html" width="100%" height="500"/>
+      <embed type="text/html" src="map.html" width="100%" height="500"/>
     </div>
 }
 
