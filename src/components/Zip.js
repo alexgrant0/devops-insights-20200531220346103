d5837121a0a0da6ade8ff8926629940582afe512
Alex Grant <alexgrant0@gmail.com>
2020-06-03 02:28:02 +0000

    Reformatted

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -17,35 +17,32 @@ function Zip(props) {
     };
 
     return (
-	    <div>
-	        <div className="col-sm-4">
-	            <div className="row">
-	                <div className="col-sm-10">
-	                    <style jsx="true">{`
-	                        .form-control::-webkit-input-placeholder {
-	                            color: #ddd;
-	                        }
-	                    `}
-	                    </style>
-	                    <input 
-	                        type="text" 
-	                        className="form-control" 
-	                        id="usr" 
-	                        placeholder="NZ City Name"
-	                        onKeyPress={(event) => {
-	                            if (event.key === "Enter") {
-	                                validate(event);
-	                            }
-	                        }}
-	                    ></input>   
-	                </div>
-	            </div>
-	            <div className="pl-3 row">
-	                <div className="text-danger small">{ validationError }</div>
-	            </div>
-	        </div>
-	        
-	    </div>
+        <div className="col-sm-4">
+            <div className="row">
+                <div className="col-sm-10">
+                    <style jsx="true">{`
+                        .form-control::-webkit-input-placeholder {
+                            color: #ddd;
+                        }
+                    `}
+                    </style>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="usr" 
+                        placeholder="NZ City Name"
+                        onKeyPress={(event) => {
+                            if (event.key === "Enter") {
+                                validate(event);
+                            }
+                        }}
+                    ></input>   
+                </div>
+            </div>
+            <div className="pl-3 row">
+                <div className="text-danger small"> { validationError } </div>
+            </div>
+        </div>
     );
 }
 
