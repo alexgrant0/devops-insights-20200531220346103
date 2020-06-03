c72904f2b5820af6a5480a61d76f76ae936bfef8
Alex Grant <alexgrant0@gmail.com>
2020-06-03 05:09:25 +0000

    Changed location of script

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -15,6 +15,14 @@ function Zip(props) {
             props.onZipChange(event.target.value);
         }
     };
+    
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZXF1aW5uMTAiLCJhIjoiY2lxdjFpa3M1MDBlY2Zzbmh3Mjk1MTl1NSJ9.JJwjExkgWnX3QUNiN8c_9Q';
+	var map = new mapboxgl.Map({
+						  container: 'map', // HTML container id
+						  style: 'mapbox://styles/mapbox/streets-v9', // style URL
+						  center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
+						  zoom: 13
+						});
 
     return (
     	
@@ -45,16 +53,6 @@ function Zip(props) {
             </div>
             <div id='map'></div>
             <script src='https://api.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.js'></script>
-            <script>
-            	mapboxgl.accessToken = 'pk.eyJ1IjoiZXF1aW5uMTAiLCJhIjoiY2lxdjFpa3M1MDBlY2Zzbmh3Mjk1MTl1NSJ9.JJwjExkgWnX3QUNiN8c_9Q';
-				var map = new mapboxgl.Map({
-									  container: 'map', // HTML container id
-									  style: 'mapbox://styles/mapbox/streets-v9', // style URL
-									  center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
-									  zoom: 13
-									});
-
-            </script>
         </div>
     );
 }
