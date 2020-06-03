7e74d3c7b0efa63e2579b587b6a79a3497214671
Alex Grant <alexgrant0@gmail.com>
2020-06-03 04:34:51 +0000

    Changed mapbox functionality

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -15,15 +15,6 @@ function Zip(props) {
             props.onZipChange(event.target.value);
         }
     };
-    
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZXF1aW5uMTAiLCJhIjoiY2lxdjFpa3M1MDBlY2Zzbmh3Mjk1MTl1NSJ9.JJwjExkgWnX3QUNiN8c_9Q';
-	var map = new mapboxgl.Map({
-						  container: 'map', // HTML container id
-						  style: 'mapbox://styles/mapbox/streets-v9', // style URL
-						  center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
-						  zoom: 13
-						});
-
 
     return (
         <div className="col-sm-4">
@@ -52,7 +43,16 @@ function Zip(props) {
                 <div className="text-danger small"> { validationError } </div>
             </div>
             <div id='map'></div>
-            <script src='https://api.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.js'></script>
+            <script src='https://api.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.js'>
+            	mapboxgl.accessToken = 'pk.eyJ1IjoiZXF1aW5uMTAiLCJhIjoiY2lxdjFpa3M1MDBlY2Zzbmh3Mjk1MTl1NSJ9.JJwjExkgWnX3QUNiN8c_9Q';
+				var map = new mapboxgl.Map({
+									  container: 'map', // HTML container id
+									  style: 'mapbox://styles/mapbox/streets-v9', // style URL
+									  center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
+									  zoom: 13
+									});
+
+            </script>
 			<link href='https://api.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.css' rel='stylesheet' />
         </div>
     );
