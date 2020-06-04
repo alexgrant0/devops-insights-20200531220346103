ddce6a5244cc3db028111bc30f59edc3c3007cfd
Alex Grant <alexgrant0@gmail.com>
2020-06-04 06:51:52 +0000

    Testing db stuff

--- public/map.js
+++ public/map.js
@@ -11,6 +11,7 @@ function initMap(){
     var lat = myLatLng.lat();
     var lng = myLatLng.lng();
     getInformation(lat, lng);
+    createConnection();
   })  
 	
 	var marker;
@@ -38,7 +39,43 @@ function initMap(){
 		console.log(Http.responseText);
 		window.localStorage.setItem('weather', Http.responseText);		
 	}
-  }
-  
-  
+  }  
 }
+
+function createConnection(){
+	var db2id = {
+	  "db": "BLUDB",
+	  "dsn": "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=mrb12002;PWD=hl8h@gf2194zdvqf;",
+	  "host": "dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net",
+	  "hostname": "dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net",
+	  "https_url": "https://dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net",
+	  "jdbcurl": "jdbc:db2://dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net:50000/BLUDB",
+	  "parameters": {},
+	  "password": "hl8h@gf2194zdvqf",
+	  "port": 50000,
+	  "ssldsn": "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;PORT=50001;PROTOCOL=TCPIP;UID=mrb12002;PWD=hl8h@gf2194zdvqf;Security=SSL;",
+	  "ssljdbcurl": "jdbc:db2://dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net:50001/BLUDB:sslConnection=true;",
+	  "uri": "db2://mrb12002:hl8h%40gf2194zdvqf@dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net:50000/BLUDB",
+	  "username": "mrb12002"
+	}
+	
+	var api = '/dbapi/v3';
+	var host = 'https://dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net' + api;
+	
+	var userinfo = {
+		"userid" : "mrb12002",
+		"password" : "hl8h@gf2194zdvqf"
+	}
+	
+	const userAction = async () => {
+	  const response = await fetch(host, {
+	    method: 'POST',
+	    body: userInfo
+	  });
+	  const myJson = await response.json();
+	  console.log(myJson);
+	}
+	
+	
+}
+
