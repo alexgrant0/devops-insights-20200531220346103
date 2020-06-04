2c4fece78eb633e01a787e0e0f34046aba0dce3d
Alex Grant <alexgrant0@gmail.com>
2020-06-04 02:48:31 +0000

    Removed semicolons

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -25,12 +25,12 @@ function AppContainer(props) {
         <div>
             <div className="row mt-4">
                 <div className="col-sm-4"></div>
-                <Zip onZipChange={JSON.parse(window.localStorage.getItem('weather'));} clearResponse={clearResponse}/>
+                <Zip onZipChange={JSON.parse(window.localStorage.getItem('weather'))} clearResponse={clearResponse}/>
                 <div className="col-sm-4"></div>
             </div>
             <div className="row mt-4">
                 <div className="col-sm-2"></div>
-                <ZipResponse responseData={JSON.parse(window.localStorage.getItem('weather'));} clearResponse={clearResponse}/>
+                <ZipResponse responseData={JSON.parse(window.localStorage.getItem('weather'))} clearResponse={clearResponse}/>
                 <div className="col-sm-2"></div>
             </div>
         </div>
